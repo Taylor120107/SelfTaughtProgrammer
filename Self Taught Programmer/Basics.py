c19634d53cc6b1173f32b7e336f0eaef9a97752a
Taylor120107 <LukeT12@msn.com>
2023-06-16 09:21:43 +0100

    First 2 chapters

---
+++ Self Taught Programmer/Basics.py
@@ -0,0 +1,29 @@
+# for i in range(10):
+
+#     print ("Hello World!")
+
+# print(2 * 2)
+
+# x = 100
+# print(x)
+# x=200
+# print(x)
+
+# x = 50
+# y = 40
+# z = x + y
+# print (z)
+# a = x* y
+# print (a)
+
+# x = 10
+# x += 1
+# print (x)
+
+# x = 10
+# x -= 1
+# print (x)
+
+print ("Hello World)
+
+
